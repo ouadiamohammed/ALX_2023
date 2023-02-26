9d5648665f28c36907965af35125bd388081b44d
mouadia <ouadia.mohammed@outlook.de>
2023-02-26 21:12:41 +0100

    c program for task 0

---
+++ Task_0.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int sum = 0;
+    int n = 0;  //in this case n is to know how many numbers the user wanna give
+    int number;
+    int i = 1;
+    printf("please montion how many numbers you wanna give!\n");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        printf("please give a number!\n");
+        scanf("%d", &number);
+        sum = sum + (number * number);
+        i++;
+    }
+    printf("the sum is %d", sum);
+    return 0;  
+}
